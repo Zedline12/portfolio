e7bef7b476c5e9f1223f851ad60c9e54ea45837b
Bedo <bedomohamed307@gmail.com>
2023-12-30 07:23:12 +0200

    tech update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,7 @@ $.fn.isInViewport = function() {
 
   var viewportTop = $(window).scrollTop();
   var viewportBottom = viewportTop + $(window).height();
-
+  
   return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 var $loader = document.querySelector('.loader')
@@ -12,7 +12,6 @@ var $loader = document.querySelector('.loader')
 window.onload = function() {
  
 };
-
 
 document.querySelector('.btn').addEventListener('click', function () {
   $loader.classList.add('loader--active')
@@ -22,14 +21,38 @@ document.querySelector('.btn').addEventListener('click', function () {
   }, 5000)
 })
 
-
+var position = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
   $(window).scroll(function() {
-    if ($('#desimg').isInViewport()) {
-    $("#desrow").addClass("fadeinout")
+    //men foq le ta7t
+      if($("#desimg").isInViewport()){
+        $("#desrow").addClass("fadeinout")
+      }
+    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+    if (scrollTop > position) {
+      //downwards
+      if (scrollTop>=1743) {
+        
+        document.body.style.background ='black';
+        scrolleddown=1
+        
+        $(".anav").css("color","white")
+        } 
+    } else {
+      //upwards
+      if(scrollTop<=2500){
+        document.body.style.background ='#d9d9d9';
+        $(".anav").css("color","black")
+       scrolledtop=1;
+      }
+    }
+    position = scrollTop;
    
-    } 
+ 
+ 
+
      if($("#loader").isInViewport()) {
       $loader.classList.remove('loader--active')
     }
+
     });
   
